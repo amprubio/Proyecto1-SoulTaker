0ea69f807adfca39a076e3124fc07544023d232f
Juandiem <juandiem@ucm.es>
2018-04-05 22:38:36 +0200

    Enemy Properties
    Añadido scripts de almas y oscuridad y una version mejorada de las características de los enemigos

--- Proyecto1-1.0  (NEW)/Proyecto1-1.0  (NEW)/Assets/Scripts/Darkness.cs
+++ Proyecto1-1.0  (NEW)/Proyecto1-1.0  (NEW)/Assets/Scripts/Darkness.cs
@@ -4,31 +4,42 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Darkness : MonoBehaviour {
-
     private float darkness = 0f;
     private Text darknessCount;
+    private GameObject enemy;
+    private EnemyProperties enemyDarkness;
 
-
-	// Use this for initialization
-	void Start () 
+    // Use this for initialization
+    void Start ()
     {
         SetInitialReferences();
         InitialDarkness();
-	}
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    void SetInitialReferences()
+	void Update ()
     {
-        darknessCount = GameObject.Find("DarknessCount").GetComponent<Text>();
-    }
+        AddDarkness();
+	}
 
     void InitialDarkness()
     {
         if (darknessCount != null)
             darknessCount.text = ("" + darkness);
+    }
+    
+    void SetInitialReferences()
+    {
+        darknessCount = GameObject.Find("DarknessCount").GetComponent<Text>();
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
+        enemyDarkness = GameObject.FindGameObjectWithTag("Enemy").GetComponent<EnemyProperties>();
+    }
+
+    void AddDarkness()
+    {
+        if (enemy == null)
+            darkness += enemyDarkness.souls;
+
+        darknessCount.text = ("" + darkness);
     }
 }
--- Proyecto1-1.0  (NEW)/Proyecto1-1.0  (NEW)/Assets/Scripts/Souls.cs
+++ Proyecto1-1.0  (NEW)/Proyecto1-1.0  (NEW)/Assets/Scripts/Souls.cs
@@ -4,24 +4,30 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Souls : MonoBehaviour {
+
     private float souls = 0f;
     private Text soulsCount;
+    private GameObject enemy;
+    private EnemyProperties enemySouls;
 
-	// Use this for initialization
-	void Start () 
+    // Use this for initialization
+    void Start ()
     {
         SetInitialReferences();
         InitialSouls();
-	}
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+            AddSouls();
 	}
 
     void SetInitialReferences()
     {
         soulsCount = GameObject.Find("SoulsCount").GetComponent<Text>();
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
+        enemySouls = GameObject.FindGameObjectWithTag("Enemy").GetComponent<EnemyProperties>();
     }
 
     void InitialSouls()
@@ -29,4 +35,12 @@ public class Souls : MonoBehaviour {
         if (soulsCount != null)
             soulsCount.text = ("" + souls);
     }
+
+    void AddSouls()
+    {
+        if (enemy == null)
+            souls += enemySouls.souls;
+
+        soulsCount.text = ("" + souls);
+    } 
 }
