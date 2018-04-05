ccc3ff2d12d0f6062cbda74914bed9cd0f6c9ae1
Kreksu <37438602+Kreksu@users.noreply.github.com>
2018-04-05 20:48:55 +0200

    He añadido los prefabs funcionales del boom, el floof y la explosión (Cada uno con sus respectivos sprites y scripts)

---
+++ Proyecto1-1.0  (NEW)/Proyecto1-1.0  (NEW)/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public GameObject enemyGraphic;
+    bool canFlip = true;
+    bool facingRight = false;
+    float flipTime = 5f;
+    float nextFlipChance = 0f;
+
+    public float chargeTime;
+    public float enemySpeed;
+    float startChargeTime;
+
+    Rigidbody2D enemyRB;
+
+    // Use this for initialization
+    void Start()
+    {
+        enemyRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextFlipChance)
+        {
+            if (Random.Range(0, 10) >= 2)
+            {
+                flipFacing();
+            }
+            int randomNumber = Random.Range(0, 10);
+            if (randomNumber >= 2)
+            {
+                if (facingRight)
+                {
+                    enemyRB.velocity = new Vector2((enemySpeed - 2), 0f);
+                }
+                else
+                {
+                    enemyRB.velocity = new Vector2((-enemySpeed + 2), 0f);
+                }
+            }
+            else
+            {
+                enemyRB.velocity = new Vector2(0f, 0f);
+            }
+            nextFlipChance = Time.time + flipTime;
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+
+            if (facingRight && other.transform.position.x < transform.position.x)
+            {
+                flipFacing();
+            }
+            else if (!(facingRight) && other.transform.position.x > transform.position.x)
+            {
+                flipFacing();
+            }
+            canFlip = false;
+
+            startChargeTime = Time.time + chargeTime;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            if (startChargeTime < Time.time)
+            {
+                if (!facingRight)
+                {
+                    enemyRB.AddForce(new Vector2(-1, 0) * enemySpeed);
+                }
+                else enemyRB.AddForce(new Vector2(+1, 0) * enemySpeed);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            canFlip = true;
+            enemyRB.velocity = new Vector2(0f, 0f);
+        }
+    }
+
+    void flipFacing()
+    {
+        if (canFlip)
+        {
+            float facingX = enemyGraphic.transform.localScale.x;
+            facingX *= -1f;
+            enemyGraphic.transform.localScale = new Vector3(facingX, enemyGraphic.transform.localScale.y, enemyGraphic.transform.localScale.z);
+            facingRight = !facingRight;
+        }
+
+    }
+}
---
+++ Proyecto1-1.0  (NEW)/Proyecto1-1.0  (NEW)/Assets/Scripts/Explosion.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+
+    public Transform target;
+    public float attack1Range = 0.1f;
+    public GameObject objectExplosion;
+    GameObject explosion;
+    bool activaExplosion = false;
+
+    public void Update()
+    {
+        InvocaBoom();
+    }
+
+    public void InvocaBoom()
+    {
+        if (Vector3.Distance(transform.position, target.position) < attack1Range)
+        {
+            Invoke("ActivaExplosion", 1f);
+            Boom();
+        }
+    }
+
+    public void Boom()
+    {
+        if (Vector3.Distance(transform.position, target.position) < attack1Range && activaExplosion == true)
+        {
+            explosion = GameObject.Instantiate(objectExplosion);
+            explosion.transform.position = transform.position;
+            Destroy(gameObject);
+        }
+        activaExplosion = false;
+    }
+
+    public void ActivaExplosion()
+    {
+        activaExplosion = true;
+    }
+}
