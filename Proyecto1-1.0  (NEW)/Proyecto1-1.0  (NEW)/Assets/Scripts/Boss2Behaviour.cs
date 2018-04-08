d15858a7954fe5b1d046ff6b04e1e6029a3348a4
dvega03 <dvega03@users.noreply.github.com>
2018-04-08 16:54:59 +0200

    Rama Boss2 creada con los cambio en la descripción
    Añadido sprite boss.
    Funcionalidad disparo triple, movimiento a través de unos puntos predeterminados.

---
+++ Proyecto1-1.0  (NEW)/Proyecto1-1.0  (NEW)/Assets/Scripts/Boss2Behaviour.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss2Behaviour : MonoBehaviour {
+
+    [Header("Puntos para el moviento del Boss")]
+    public Transform[] WayPoints = new Transform[7];
+    public Transform PuntoSpawn;
+    [Header("Atributos del Boss")]
+    [Header("El anguloAperturaDisparo debe ser un número menor a 45")]
+    [Header("La rotaciónLocalProyectil se encarga de rotar el sprite,")]
+    [Header("si se pone un valor positivo el angulo se hace mas pequeño.")]
+    public int anguloAperturaDisparo;
+    public int rotaciónLocalProyectil;
+    public int movementSpeed;
+    public GameObject Proyectil;
+    public SpriteRenderer bossSprite;
+    
+    [HideInInspector]
+    public static bool Flip;
+
+    
+    public int currentWayPoint = 0;
+    private Transform nextWayPoint;
+    private int varSpeed;
+    public float temp = 3f;
+    public bool IsShooting = false;
+    public Vector3 tempPos = new Vector3();
+    public Vector3 posOffset = new Vector3();
+    private float amplitude = 0.01f;
+    private float frequency = 0.5f;
+    
+
+
+           
+    
+    
+    void Start ()
+    {
+        varSpeed = movementSpeed;
+        
+        if (movementSpeed == 0) movementSpeed = 5;
+	}
+	
+	
+
+	void Update ()
+    {
+        if(currentWayPoint < WayPoints.Length)
+        {
+            if (nextWayPoint == null)
+                nextWayPoint = WayPoints[currentWayPoint];
+           switch(currentWayPoint)
+           {
+                case 2:
+                case 6:
+                    if (!IsShooting)
+                    {
+                        Shoot();
+                        IsShooting = true;
+                    }
+                    if (temp > 0 && IsShooting == true)
+                    {
+                        temp = temp - Time.deltaTime;
+                        IsShooting = true;
+                    }
+                    else
+                    {
+                        IsShooting = false;
+                        temp = 3f;
+                    }
+                    Idle();
+                    //MoveBoss(movementSpeed);
+                    break;
+                
+                default:
+                    MoveBoss(movementSpeed);
+                    break;
+
+                    
+
+
+           }
+
+            
+        }
+	}
+    void MoveBoss(int sp)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, nextWayPoint.position, sp * Time.deltaTime);
+
+        if(transform.position == nextWayPoint.position)
+        {
+            
+            currentWayPoint = (currentWayPoint + 1) % WayPoints.Length;
+            nextWayPoint = WayPoints[currentWayPoint];
+        }
+    }
+    private void Shoot()
+    {
+        int x = 0;
+        int anguloBase = -45 + anguloAperturaDisparo;
+        int desfaseAngulo = -anguloBase;
+        Flip = bossSprite.flipX;
+        for (int i = 0; i < 3; i++)
+        {
+            GameObject Proyectiles = (GameObject)Instantiate(Proyectil, PuntoSpawn.transform.position, Quaternion.identity);
+            if (anguloBase + (desfaseAngulo * i) > 0)
+            {
+                x = -rotaciónLocalProyectil;
+            }
+            else if ((anguloBase + (desfaseAngulo * i)) == 0)
+            {
+                x = 0;
+            }
+            else
+            {
+                x = rotaciónLocalProyectil;
+            }
+
+            Proyectiles.transform.rotation = Quaternion.Euler(0, 0, anguloBase + (desfaseAngulo * i) + x);
+
+        }
+    }
+
+    private void Idle()
+    {
+        tempPos = transform.position;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+        transform.position = tempPos;
+    }
+
+}
---
+++ Proyecto1-1.0  (NEW)/Proyecto1-1.0  (NEW)/Assets/Scripts/ProyectilBoss.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProyectilBoss : MonoBehaviour {
+
+    public int speed;
+	private Vector3 vectorDirector;
+    public float angulo;
+    public float seno;
+    public float coseno;
+    private Vector3 target;
+    private SpriteRenderer render;
+
+    
+
+	void Start ()
+    {
+        render = gameObject.GetComponent<SpriteRenderer>();
+        angulo = transform.rotation.z;
+        target = new Vector3(coseno = Mathf.Cos(angulo), seno = Mathf.Sin(angulo), 0).normalized;
+	}
+	
+	
+	void Update ()
+    {
+        int direccion;
+        if (Boss2Behaviour.Flip == false)
+        {
+            direccion = -1;
+            render.flipX = false;
+        }
+        else
+        {
+            direccion = 1;
+            render.flipX = true;
+        }
+        transform.position += target * speed * direccion * Time.deltaTime;
+	}
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
