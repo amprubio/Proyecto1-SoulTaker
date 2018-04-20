046fe82d8002c59070bd5d736e9f1c24c27e7b03
Kreksu <37438602+Kreksu@users.noreply.github.com>
2018-04-20 11:56:01 +0200

    Actualizando mi directorio

--- Proyecto1-1.0  (NEW)/Proyecto1-1.0  (NEW)/Assets/Scripts/Explosion.cs
+++ Proyecto1-1.0  (NEW)/Proyecto1-1.0  (NEW)/Assets/Scripts/Explosion.cs
@@ -5,39 +5,48 @@ using UnityEngine;
 public class Explosion : MonoBehaviour
 {
 
-    public Transform target;
-    public float attack1Range = 0.1f;
+    private Transform target;
+    public float attack1Range = 1f;
     public GameObject objectExplosion;
     GameObject explosion;
-    bool activaExplosion = false;
+    float time = 0;
+    public float t = 2;
+    int estado = 1;
+
+    private void Start()
+    {
+        target = GameObject.Find("Player").GetComponent<Transform>();
+    }
 
     public void Update()
     {
-        InvocaBoom();
-    }
-
-    public void InvocaBoom()
-    {
-        if (Vector3.Distance(transform.position, target.position) < attack1Range)
+        if (estado == 1)
         {
-            Invoke("ActivaExplosion", 1f);
-            Boom();
+            time = 0;
+            if (Vector3.Distance(transform.position, target.transform.position) < attack1Range)
+            {
+                estado = 2;
+            }
+        }
+        else if (estado == 2)
+        {
+            time += Time.deltaTime;
+            if (Vector3.Distance(transform.position, target.transform.position) > attack1Range)
+            {
+                estado = 1;
+            }
+            if (time >= t)
+            {
+                Boom();
+            }
         }
     }
+
 
     public void Boom()
     {
-        if (Vector3.Distance(transform.position, target.position) < attack1Range && activaExplosion == true)
-        {
-            explosion = GameObject.Instantiate(objectExplosion);
-            explosion.transform.position = transform.position;
-            Destroy(gameObject);
-        }
-        activaExplosion = false;
-    }
-
-    public void ActivaExplosion()
-    {
-        activaExplosion = true;
+        explosion = GameObject.Instantiate(objectExplosion);
+        explosion.transform.position = transform.position;
+        Destroy(gameObject);
     }
 }
