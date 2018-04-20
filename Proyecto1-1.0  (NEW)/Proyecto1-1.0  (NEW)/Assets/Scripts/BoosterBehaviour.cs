e0dd2b78a12a8a4785cf0058668e4a60d08743cc
dvega03 <dvega03@users.noreply.github.com>
2018-04-20 02:04:35 +0200

    Añadido la mayoria de la funcionalidad de la Tienda

---
+++ Proyecto1-1.0  (NEW)/Proyecto1-1.0  (NEW)/Assets/Scripts/BoosterBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoosterBehaviour : MonoBehaviour {
+
+    public int amountDarkness;
+    public int amountSouls;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        GameManager.instance.SubsDarkness(amountDarkness);
+        GameManager.instance.SubsSouls(amountSouls);
+    }
+}
