ed9c4423972e54477ca72a43f708831138c1d538
dvega03 <dvega03@ucm.es>
2018-04-26 02:55:35 +0200

    Solucionando problemas del boss1

--- Proyecto1-1.0  (NEW)/Proyecto1-1.0  (NEW)/Assets/Scripts/Boss1Behaviour.cs
+++ Proyecto1-1.0  (NEW)/Proyecto1-1.0  (NEW)/Assets/Scripts/Boss1Behaviour.cs
@@ -1,6 +1,7 @@
 ﻿
 //arreglar parada en shoot();
 
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,73 +11,215 @@ public class Boss1Behaviour : MonoBehaviour {
 	public GameObject player;
 	public SpriteRenderer bossSprite;
 	private Transform nextWayPoint;
+    public float speed = 1f;
 	public GameObject Proyectil;
 	public int numberProyectiles = 5;
 	public int currentWayPoint = 0;
-	public int timeofshoot=10;
+	public int timeofshoot=5;
 	public static bool Flip;
 	public Transform PuntoSpawn;
-	private Animator anim;
 	private float angleOffset;
 	private float minAngle;
+    private int incr = 1;
+    Animator anim;
+    private float varSpeed;
+    Vector3 tempPos;
+    //temps
+    float tempAnim;
+    float tempIdle = 3f;
+    float tempShooting;
+    bool IsShooting = false;
+    bool IsIdle = false;
+    public int random;
 
-	// Use this for initialization
-	void Start () {
-		anim = GetComponent<Animator> ();
+    System.Random rnd= new System.Random();
+
+    void Start () {
 		Collider2D colBoss = gameObject.GetComponent<Collider2D>();
-		nextWayPoint = WayPoints [1];
+        transform.position = WayPoints[0].transform.position;
+        nextWayPoint = WayPoints[1].transform;
+        anim = gameObject.GetComponent<Animator>();
+        varSpeed = speed;
 	}
 
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+        Fase2();
 
-		MoveBoss ();
-	}
-	void MoveBoss()
-	{	
-		//Le pone la correa al good boy
-		Debug.DrawLine(player.transform.position, bossSprite.transform.position, Color.black);
-		//Va para el nodo siguiente
-		bossSprite.transform.position = Vector3.MoveTowards(bossSprite.transform.position, nextWayPoint.transform.position, 3 * Time.deltaTime);
-		anim.SetBool("ataque",true);
-		//IMPORTANTE TU NODO NUMERO 6 VA A SER EN EL QUE QUIERES QUE GIRE DE DERECHA A IZQUIERDA.
-		//MIS NODOS ESTAN PUESTOS DE ESTA MANERA 7...6...1...2.5...4...3
-		if (currentWayPoint == 6) {
-			bossSprite.flipX = false;
-			currentWayPoint = 0;
-			nextWayPoint = WayPoints[currentWayPoint];
+        random = rnd.Next(0, 11);
+        if (currentWayPoint == 0)
+        {
+            if(random < 5)
+            {
+                Fase1();
+            }
+            
+        }
 
-		}
-		//TU NODO 3 VA A SER EN EL QUE QUIERES QUE GIRE DE DERECHA A IZQUIERDA
-		if (currentWayPoint == 3) {
-			bossSprite.flipX = true;
-			Debug.Log ("Flipo");
-			nextWayPoint = WayPoints[currentWayPoint];
 
-		}
 
-	
-		InvokeRepeating ("Shoot", 5, 20);
+    }
+	void MoveBoss(float speedM)
+	{
+        transform.position = Vector3.MoveTowards(transform.position, nextWayPoint.position, speedM * Time.deltaTime);
 
-		currentWayPoint = (currentWayPoint + 1) % WayPoints.Length;
-		nextWayPoint = WayPoints[currentWayPoint];
+        
 
-	}
-	private void Shoot ()
-	{	anim.SetTrigger ("disparo");
-		Flip = bossSprite.flipX;
-		Debug.DrawLine (WayPoints [currentWayPoint].position, nextWayPoint.position, Color.cyan);
-		float count = 0;
-		//esto no sirve en verdad
-		while (count < timeofshoot) {
+        if (transform.position == nextWayPoint.position)
+        {
+            if(currentWayPoint == 0)
+            {
+                incr = 1;
+            }
+            else if(currentWayPoint == 5 )
+            {
+                incr = -1;
+            }
 
-			anim.SetTrigger ("descanso");
+            currentWayPoint = currentWayPoint + incr;
+            nextWayPoint = WayPoints[currentWayPoint];
+        }
 
-			bossSprite.transform.position = Vector3.MoveTowards (WayPoints [currentWayPoint].transform.position, WayPoints [currentWayPoint - 1].transform.position, 0.001f * Time.deltaTime);
+    }
+	private void Shoot()
+	{
 
-			count += Time.deltaTime;
+        
+        //Rotacion del boss hacia el player
+        Vector3 direcc = player.transform.position - PuntoSpawn.transform.position;
+        float rotz = LookAtPlayer(bossSprite.flipX, direcc);
+        
 
-		}	
-		MoveBoss ();
-	}
+        angleOffset = 90 / numberProyectiles;
+        minAngle = rotz + ((numberProyectiles / 2) * angleOffset);
+
+        //Hacia positivos min angulo = rotz - (numberProyectiles / 2) * angleOffset
+        for (int i = 0; i < numberProyectiles; i++)
+        {
+            GameObject Proyectiles = (GameObject)Instantiate(Proyectil, PuntoSpawn.transform.position, Quaternion.identity);
+            //if (rotz + (angleOffset * (i+1)) > 0)
+            //{
+            //    x = -rotaciónLocalProyectil;
+            //}
+            //else if ((rotz + (angleOffset * i)) == 0)
+            //{
+            //    x = 0;
+            //}
+            //else
+            //{
+            //    x = rotaciónLocalProyectil;
+            //}
+            Proyectiles.transform.rotation = Quaternion.Euler(0, 0, minAngle - (angleOffset * i));
+
+        }
+    }
+
+    void Fase1()
+    {
+        switch(currentWayPoint)
+        {
+            case 0:
+
+                //Animacion de carga
+               
+                MoveBoss(speed);
+                break;
+
+            default:
+                //Animacion de andar
+                varSpeed += 3;
+                MoveBoss(varSpeed);
+                break;
+
+        }
+
+    }
+    void Fase2()
+    {
+        switch (currentWayPoint)
+        {
+            case 0:
+                bossSprite.flipX = false;
+                MoveBoss(speed);
+                RestartValues();
+                
+                
+                break;
+            case 1:
+            case 4:
+
+                //Animacion de disparo
+                if (!IsShooting)
+                {
+                    Shoot();
+
+
+                    IsShooting = true;
+                }
+                if (tempShooting > 0 && IsShooting == true)
+                {
+                    tempShooting = tempShooting - Time.deltaTime;
+                    IsShooting = true;
+                }
+                else
+                {
+                    IsShooting = false;
+                    tempShooting = 3f;
+                }
+                if (tempIdle > 0)
+                {
+                    
+                    tempIdle = tempIdle - Time.deltaTime;
+                    IsIdle = true;
+                }
+                else
+                {
+                    MoveBoss(speed);
+                }
+                break;
+
+            case 6:
+                bossSprite.flipX = true;
+                MoveBoss(speed);
+                RestartValues();
+                break;
+
+            default:
+                //Animacion de andar
+                RestartValues();
+                MoveBoss(speed);
+                break;
+
+        }
+
+    }
+    
+
+    private float LookAtPlayer(bool flip, Vector3 angBase)
+    {
+
+        angBase.Normalize();
+        float rotz = Mathf.Atan2(angBase.y, angBase.x) * Mathf.Rad2Deg;
+
+        if (!flip)
+        {
+            rotz = rotz + 180;
+        }
+
+
+        return rotz;
+
+    }
+
+    private void RestartValues()
+    {
+        IsShooting = false;
+        IsIdle = false;
+        tempIdle = 3f;
+        tempShooting = 3f;
+        
+    }
+
 }
+	
--- Proyecto1-1.0  (NEW)/Proyecto1-1.0  (NEW)/Assets/Scripts/BulletBoss1Behaviour.cs
+++ Proyecto1-1.0  (NEW)/Proyecto1-1.0  (NEW)/Assets/Scripts/BulletBoss1Behaviour.cs
@@ -16,12 +16,12 @@ public class BulletBoss1Behaviour : MonoBehaviour {
 	private SpriteRenderer render;
 	private float tempDestroy = 10f;
 	private bool Flipped;
-	public Transform player;
+
 
 
 	void Start ()
 	{
-		player = GetComponent<Transform> ();
+
 		Flipped = Boss1Behaviour.Flip; 
 		render = gameObject.GetComponent<SpriteRenderer>();
 		angulo = transform.rotation.z;
@@ -31,10 +31,20 @@ public class BulletBoss1Behaviour : MonoBehaviour {
 
 	void Update ()
 	{
-		Vector3.MoveTowards( render.transform.position,player.position, speed  * Time.deltaTime);
+		int direccion;
+		if (!Flipped)
+		{
+			direccion = -1;
+			render.flipX = false;
+		}
+		else
+		{
+			direccion = 1;
+			render.flipX = true;
+		}
+		transform.position += transform.right * speed * direccion * Time.deltaTime;
 		tempDestroy = tempDestroy - Time.deltaTime;
-		if (tempDestroy < 0) 
-			Destroy(gameObject);
+		if (tempDestroy < 0) Destroy(gameObject);
 	}
 
 	private void OnBecameInvisible()
