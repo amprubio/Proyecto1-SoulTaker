3f44a314e8e807468fa3ffb7ff5ac366031c48d0
Juandiem <juandiem@ucm.es>
2018-04-26 15:52:41 +0200

    Creado Checkpoints y Respawn

---
+++ Proyecto1-1.0  (NEW)/Proyecto1-1.0  (NEW)/Assets/Scripts/LevelScripts/LevelManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public GameObject currentCheckpoint;
+    private MovementController player;
+    public static LevelManager instance;
+
+
+    void Awake()
+    {
+
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    // Use this for initialization
+    void Start ()
+    {
+        player = FindObjectOfType<MovementController>();
+	}
+
+    //Call this method whenever u guys want to respawn player, no matter if u collide with Water or Spikes. Dont use it for player.health = 0, use load the scene u want indeed.
+
+    public void RespawnPlayer()  
+    {
+        player.transform.position = currentCheckpoint.transform.position;
+    }
+}
---
+++ Proyecto1-1.0  (NEW)/Proyecto1-1.0  (NEW)/Assets/Scripts/LevelScripts/Water_or_Spikes.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water_or_Spikes : MonoBehaviour {
+
+    //Attach this script to a Water or Spike Object 
+
+    private LevelManager levelManager;
+
+    void Start()
+    {
+        levelManager = FindObjectOfType<LevelManager>();
+    }
+    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name == "Player")
+            levelManager.RespawnPlayer();
+    }
+}
