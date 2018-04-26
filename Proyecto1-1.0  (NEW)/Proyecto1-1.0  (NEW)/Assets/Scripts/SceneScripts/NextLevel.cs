0513d28b7a063f2f0583c081414f32c5044e2883
Juandiem <juandiem@ucm.es>
2018-04-26 13:58:43 +0200

    Creado Menu de Pausa

--- Proyecto1-1.0  (NEW)/Proyecto1-1.0  (NEW)/Assets/Scripts/SceneScripts/NextLevel.cs
+++ Proyecto1-1.0  (NEW)/Proyecto1-1.0  (NEW)/Assets/Scripts/SceneScripts/NextLevel.cs
@@ -10,4 +10,6 @@ public class NextLevel : MonoBehaviour {
         if (other.gameObject.name == "Player")
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+   
 }
---
+++ Proyecto1-1.0  (NEW)/Proyecto1-1.0  (NEW)/Assets/Scripts/SceneScripts/PauseManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour {
+
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public Animator pauseMenu;
+    public AnimationClip pauseOff;
+    public static PauseManager instance;
+
+    void Awake()
+    {
+
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (InputManager.StartButton())
+        {
+            if (GameIsPaused)
+                Continue();
+            else
+                Pause();
+        }
+	}
+
+    public void Continue()
+    {
+        pauseMenu.SetBool("GamePaused", false);
+        StartCoroutine(PauseAnimation());
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        pauseMenu.SetBool("GamePaused", true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    IEnumerator PauseAnimation()
+    {
+        yield return new WaitForSeconds(pauseOff.length);
+    }
+}
