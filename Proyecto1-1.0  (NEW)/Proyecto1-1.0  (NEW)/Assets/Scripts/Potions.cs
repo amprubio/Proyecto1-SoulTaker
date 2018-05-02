0c88a1a542e2063427e1505aaf211e02ebbf6df1
dvega03 <dvega03@ucm.es>
2018-05-02 21:21:17 +0200

    Implementada funcionalidad y represenatcion en la GUI de las pociones.

---
+++ Proyecto1-1.0  (NEW)/Proyecto1-1.0  (NEW)/Assets/Scripts/Potions.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Potions : MonoBehaviour {
+
+    Vida vida;
+    const int MaximumPotions = 3;
+    public GameObject healingEffect;
+    bool ActiveEffect = false;
+    public float tempEffect = 0.75f;
+    private int CurrentPotions;
+
+    public Image[] potions;
+    public Sprite[] potionGUI;
+
+	void Start ()
+    {
+        vida = gameObject.GetComponent<Vida>();
+        CurrentPotions = MaximumPotions;
+        CalculatePotions();
+
+	}
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            
+            vida.CurarVida();
+            CurrentPotions--;
+            Debug.Log("Me he tomado una pocion");
+            healingEffect.transform.position = transform.position;
+            GameObject effectGO = GameObject.Instantiate(healingEffect,transform);
+            ParticleSystem effect = effectGO.GetComponent<ParticleSystem>();
+            UpdatePotions();
+        }
+
+        if(tempEffect > 0)
+        {
+            tempEffect = tempEffect - Time.deltaTime;
+        }else
+        {
+            tempEffect = 0.75f;
+            
+
+        }
+    }
+
+    void CalculatePotions()
+    {
+        for(int i = 0; i < MaximumPotions; i++)
+        {
+            if(MaximumPotions <= i)
+            {
+                potions[i].enabled = false;
+            }
+            else
+            {
+                potions[i].enabled = true;
+            }
+        }
+
+        UpdatePotions();
+    }
+
+    void UpdatePotions()
+    {
+        bool vacio = false;
+        int j;
+        int x = 0;
+
+        for (j = 0; j < potions.Length; j++)
+        {
+            if (vacio)
+            {
+                potions[j].sprite = potionGUI[0];
+            }
+            else
+            {
+                x++;
+                x = Mathf.Clamp(x, 1, MaximumPotions);
+                if (CurrentPotions >= x)
+                {
+                    potions[j].sprite = potionGUI[1];
+                }
+                else
+                {
+                    potions[j].sprite = potionGUI[0];
+                    vacio = true;
+                }
+            }
+        }
+    }
+
+
+}
