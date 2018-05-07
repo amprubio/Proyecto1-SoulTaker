ebe97dbc6e1fc85dea99d2385593393001c64c8a
Amparo Rubio Bellón <37449976+amprubio@users.noreply.github.com>
2018-05-07 17:12:39 +0200

    parece una tienda

---
+++ Proyecto1-1.0  (NEW)/Proyecto1-1.0  (NEW)/Assets/Scripts/ShopScript/ItemsObtain.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemsObtain : MonoBehaviour {
+	//los objetos con tag de enemigo 
+
+	[Header("Atributos del objeto")]
+	[HideInInspector]
+	public bool obtained=false;
+	private float CurrentHealth;
+	public float souls;
+	public float darkness;
+	private Souls giveSouls;
+	private Darkness giveDarkness;
+
+
+	void Start()
+	{
+		SetInitialReferences();
+	}
+
+	public void OnDestroy()
+	{
+		//GameManager.instance.SubsDarkness(darkness);
+		GameManager.instance.SubsSouls(souls);
+	}
+
+
+	void SetInitialReferences()
+	{
+		CurrentHealth = 1;
+	}
+
+	public void Obtain (float i)
+	{
+		Debug.Log ("mira un objeto");
+		while (GameManager.instance.souls - souls >= 0) {
+			CurrentHealth = CurrentHealth - i;
+			if (CurrentHealth <= 0)
+				obtained = true;
+				Destroy (this.gameObject);
+			OnDestroy ();
+
+		}
+	}
+}
---
+++ Proyecto1-1.0  (NEW)/Proyecto1-1.0  (NEW)/Assets/Scripts/ShopScript/LadronBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadronBehaviour : MonoBehaviour {
+	private bool obtain;
+	// Use this for initialization
+	void Start () {
+
+		obtain = GetComponent<ItemsObtain> ().obtained;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ChangeStat ();
+	}
+	private void ChangeStat(){
+		if (obtain) {
+			GameManager.instance.Ladron ();
+		}
+	}
+}
