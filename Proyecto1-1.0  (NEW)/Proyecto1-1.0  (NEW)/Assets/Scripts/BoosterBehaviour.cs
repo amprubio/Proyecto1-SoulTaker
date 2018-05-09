7beae829b310560f060c48ccb3800e9dfb85ac0b
Amparo Rubio Bellón <37449976+amprubio@users.noreply.github.com>
2018-05-09 18:54:59 +0200

    modificaciones

--- Proyecto1-1.0  (NEW)/Proyecto1-1.0  (NEW)/Assets/Scripts/BoosterBehaviour.cs
+++ Proyecto1-1.0  (NEW)/Proyecto1-1.0  (NEW)/Assets/Scripts/BoosterBehaviour.cs
@@ -6,31 +6,29 @@ public class BoosterBehaviour : MonoBehaviour {
 
     public int amountDarkness;
     public int amountSouls;
-   
+	public Collider2D collision;   
 
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
 	}
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D()
     {
-		if(collision.gameObject.tag == "Sword" && GameManager.instance.souls-amountSouls != 0 && GameManager.instance.darkness-amountDarkness != 0)
+		if(collision.gameObject.tag == "Sword" && GameManager.instance.souls-amountSouls >= 0 && GameManager.instance.darkness-amountDarkness >= 0)
         {
+			Debug.Log ("pum");
 			GameManager.instance.SubsSouls (amountSouls);
 			GameManager.instance.SubsDarkness (amountDarkness);
             Destroy(gameObject);
-        }
+		
+		}
+
     }
 
     private void OnDestroy()
     {
-        GameManager.instance.ChangeStats(gameObject.name);
+		GameManager.instance.ChangeStats(name);
     }
 }
