f33c6a3cd304cd416e8c117f86b7019f27ef564c
Amparo Rubio Bellón <37449976+amprubio@users.noreply.github.com>
2018-05-23 17:57:46 +0200

    mergeando cosas

--- Proyecto1-1.0  (NEW)/Proyecto1-1.0  (NEW)/Assets/Scripts/ShopScript/TextoPrecioBehaviour.cs
+++ Proyecto1-1.0  (NEW)/Proyecto1-1.0  (NEW)/Assets/Scripts/ShopScript/TextoPrecioBehaviour.cs
@@ -3,29 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public class TextoPrecioBehaviour : MonoBehaviour {
-	public Text []price= new Text[3];
-	private int[] pri= new int[3];
+	public Text price;
+	private int pri;
 
-	// Use this for initialization
+
 	void Start () {
-		for (int i =0;i>=pri.Length;i++){
-			pri[i] = GameObject.Find("SpawnPoint " +"("+i+")").transform.GetChild(0).GetComponent<BoosterBehaviour>().amountSouls;
-			Initial (price [i]);
+		pri= GameObject.FindGameObjectWithTag("Spawn").transform.GetChild(0).GetComponent<BoosterBehaviour>().amountSouls;
+			Debug.Log ("aa");
+			price=GetComponent<Text> ();
+			Initial (price);
 		}
-	}
 	void Initial(Text t)
 	{
+		Debug.Log ("ayuda");
 		t = GetComponent<Text> ();
 		Debug.Log ("miaun't");
 		t.text= ("0");
 	}
-	void ReWrite(Text t, int i){
-		t.text = i.ToString ();
+
+	void Update(){
+		Debug.Log ("pf");
+		price.text = pri.ToString ();
 	}
-	// Update is called once per frame
-	void Update () {
-		for(int i =0;i>= price.Length;i++)
-			ReWrite (price[i],pri[i]);
-	}
-	}
+}
 
